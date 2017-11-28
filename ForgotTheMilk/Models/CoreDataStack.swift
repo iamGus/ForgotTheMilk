33ea38981ad0ef3ca8f4f65b85bd15c92342b001
Angus Muller <angusmuller@anguss-mbp.lan>
2017-11-28 08:49:10 +0000

    Getting some foundational elements in place

---
+++ ForgotTheMilk/Models/CoreDataStack.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataStack.swift
+//  ForgotTheMilk
+//
+//  Created by Angus Muller on 28/11/2017.
+//  Copyright © 2017 Angus Muller. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+        let container = self.persistentContainer
+        return container.viewContext
+    }()
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ImageViewer")
+        container.loadPersistentStores() { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error: \(error), \(error.userInfo)")
+            }
+        }
+        
+        return container
+    }()
+}
+
+extension NSManagedObjectContext {
+    func saveChanges() {
+        if self.hasChanges {
+            do {
+                try save()
+            } catch {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
---
+++ ForgotTheMilk/Views/ReminderCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderCell.swift
+//  ForgotTheMilk
+//
+//  Created by Angus Muller on 28/11/2017.
+//  Copyright © 2017 Angus Muller. All rights reserved.
+//
+
+import UIKit
+
+class ReminderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
