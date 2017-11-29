60c1a2953dd338f78bc9c947575b3ce724dbbe52
Angus Muller <angusmuller@anguss-mbp.lan>
2017-11-29 11:26:37 +0000

    Setup base lcoationsearch UI

---
+++ ForgotTheMilk/Models/LocationManager.swift
@@ -0,0 +1,117 @@
+//
+//  LocationManager.swift
+//  ForgotTheMilk
+//
+//  Created by Angus Muller on 29/11/2017.
+//  Copyright © 2017 Angus Muller. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+enum LocationError: Error {
+    case unknownError
+    case disallowedByUser
+    case unableToFindLocation
+}
+
+protocol LocationPermissionsDelegate: class {
+    func authorizationSucceeded()
+    func authorizationFailedWithStatus(_ status: CLAuthorizationStatus)
+}
+
+protocol LocationManagerDelegate: class {
+    func obtainedCoordinates(_ location: CLLocation)
+    func failedWithError(_ error: LocationError)
+}
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    weak var permissionsDelegate: LocationPermissionsDelegate?
+    weak var delegate: LocationManagerDelegate?
+    
+    init(delegate: LocationManagerDelegate?, permissionsDelegate: LocationPermissionsDelegate?) {
+        self.delegate = delegate
+        self.permissionsDelegate = permissionsDelegate
+        super.init()
+        manager.delegate = self
+    }
+    
+    static var isAuthorized: Bool {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse: return true
+        default: return false
+        }
+    }
+    
+    func requestLocationAuthorization() throws {
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        
+        if authorizationStatus == .restricted || authorizationStatus == .denied {
+            throw LocationError.disallowedByUser
+        } else if authorizationStatus == .notDetermined {
+            manager.requestWhenInUseAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func requestLocation() {
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // status is not determined so when status changes check:
+        if status == .authorizedWhenInUse {
+            permissionsDelegate?.authorizationSucceeded()
+        } else if status == .denied || status == .restricted {
+            permissionsDelegate?.authorizationFailedWithStatus(status)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        guard let error = error as? CLError else {
+            delegate?.failedWithError(.unknownError)
+            return
+        }
+        // If using this switch statement in real world exspand to deal with other possible error cases
+        switch error.code {
+        case .locationUnknown, .network: delegate?.failedWithError(.unableToFindLocation)
+        case .denied: delegate?.failedWithError(.disallowedByUser)
+        default: return
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            delegate?.failedWithError(.unableToFindLocation)
+            return
+        }
+        
+        // Send location coordinates to CurrentLocationController
+        delegate?.obtainedCoordinates(location)
+    }
+    
+    func getPlacemark(forLocation location: CLLocation, completionHandler: @escaping (CLPlacemark?, String?) -> ()) {
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(location, completionHandler: {
+            placemarks, error in
+            
+            if let err = error {
+                completionHandler(nil, err.localizedDescription)
+            } else if let placemarkArray = placemarks {
+                if let placemark = placemarkArray.first {
+                    completionHandler(placemark, nil)
+                } else {
+                    completionHandler(nil, "Placemark was nil")
+                }
+            } else {
+                completionHandler(nil, "Unknown error")
+            }
+        })
+        
+    }
+    
+}
